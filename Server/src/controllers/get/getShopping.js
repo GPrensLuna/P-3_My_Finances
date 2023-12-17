82dd8422d4d48ae89d479f4dc6204131f68d5722
GPrensLuna <prens.giova12@gmail.com>
2023-12-17 14:39:53 -0500

    :recycle: Mejoras de models manejo de errores

--- Server/src/controllers/get/getShopping.js
+++ Server/src/controllers/get/getShopping.js
@@ -3,11 +3,18 @@ import Shopping from '../../models/Shopping.js';
 export const getShopping = async (req, res) => {
   try {
     const shoppingInfo = await Shopping.find()
-      .populate('concept') 
-      .populate('type'); 
+      .populate({
+        path: 'concept',
+        select: 'name',
+      })
+      .populate({
+        path: 'type',
+        select: 'name',
+      });
+
     res.status(200).json(shoppingInfo);
   } catch (error) {
-    console.error('Error al obtener la información:', error);
-    res.status(500).json({ error: 'Error al procesar la solicitud' });
+    console.error('Error fetching information:', error);
+    res.status(500).json({ error: 'Error processing the request' });
   }
 };
