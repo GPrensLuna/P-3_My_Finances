2f8d88f050332f02b3100539b8117133d794f4d4
GPrensLuna <prens.giova12@gmail.com>
2023-12-17 17:53:20 -0500

    :recycle: Actulizacion e implentacion de tabla dashboard

--- Server/src/controllers/get/getShopping.js
+++ Server/src/controllers/get/getShopping.js
@@ -5,14 +5,25 @@ export const getShopping = async (req, res) => {
     const shoppingInfo = await Shopping.find()
       .populate({
         path: 'concept',
-        select: 'name',
+        select: 'name', 
       })
       .populate({
         path: 'type',
-        select: 'name',
+        select: 'name', 
       });
   
-    res.status(200).json(shoppingInfo);
+    const modifiedShoppingInfo = shoppingInfo.map(item => ({
+      _id: item._id,
+      concept: item.concept.name,
+      type: item.type.name, 
+      description: item.description,
+      value: item.value,
+      deleted: item.deleted,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt,
+    }));
+
+    res.status(200).json(modifiedShoppingInfo);
   } catch (error) {
     console.error('Error fetching information:', error);
     res.status(500).json({ error: 'Error processing the request' });
