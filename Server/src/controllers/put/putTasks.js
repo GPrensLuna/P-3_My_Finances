44da2b7499d80d99bf9155e0367b8bbb53033fd0
GPrensLuna <prens.giova12@gmail.com>
2023-12-18 21:45:20 -0500

    tasks

--- Server/src/controllers/put/putTasks.js
+++ Server/src/controllers/put/putTasks.js
@@ -1,30 +1,16 @@
 import Tasks from '../../models/Tasks.js';
 
-export const putTasks = async (req, res) => {
+export const update = async (req, res) => {
   try {
-    const { done } = req.body;
-    const id = req.params.id;
-
-    // Verificar si el ID es válido (opcional)
-    if (!isValidObjectId(id)) {
-      return res.status(400).json({ message: 'ID no válido' });
-    }
-
-    // Actualizar la tarea en la base de datos
-    const updatedTask = await Tasks.findByIdAndUpdate(
-      id,
-      { done },
-      { new: true } // Devuelve el documento modificado
-    );
-
-    // Verificar si la tarea existe
-    if (!updatedTask) {
-      return res.status(404).json({ message: 'Tarea no encontrada' });
-    }
-
-    res.status(200).json(updatedTask);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error interno del servidor' });
+    let id = req.profile;
+    req.body = { ...req.body, done: Date.now() };
+    await User.findByIdAndUpdate(id._id, { $set: req.body }, { new: true });
+    res
+      .status(200)
+      .json({ message: "the profile has been updated successfuly!" });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message,
+    });
   }
 };
