c9070835b2d5d69f48e0728643287ef987816939
GPrensLuna <prens.giova12@gmail.com>
2023-12-18 20:33:51 -0500

    tasks

--- Server/src/controllers/put/putTasks.js
+++ Server/src/controllers/put/putTasks.js
@@ -5,20 +5,11 @@ export const putTasks = async (req, res) => {
     const taskId = req.params.id;
     const { done } = req.body; 
 
-    if (done === undefined || done === null) {
-      return res.status(400).json({ error: 'The "done" attribute is required in the request body' });
-    }
-
-    // Find the task by ID and update the 'done' attribute
     const updatedTask = await Tasks.findByIdAndUpdate(
       taskId,
       { $set: { done } },
       { new: true }
     );
-
-    if (!updatedTask) {
-      return res.status(404).json({ error: 'Task not found' });
-    }
 
     res.status(200).json(updatedTask);
   } catch (error) {
