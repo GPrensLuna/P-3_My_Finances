c77eb2f26cb443dfa05e407333c4b5b33c9ee22b
GPrensLuna <prens.giova12@gmail.com>
2023-12-18 20:20:55 -0500

    tasks

--- Server/src/controllers/put/putTasks.js
+++ Server/src/controllers/put/putTasks.js
@@ -2,39 +2,26 @@ import Tasks from '../../models/Tasks.js';
 
 export const putTasks = async (req, res) => {
   try {
-    const taskId = req.params.id; 
+    const taskId = req.params.id;
+    const { done } = req.body; // Assuming the new value for the 'done' attribute is sent in the request body
 
-    const { done, deleted } = req.body;
-
-    const updateFields = {};
-
-    if (done !== undefined) {
-      updateFields.done = done;
+    // Validate if the 'done' attribute is provided in the request body
+    if (done === undefined || done === null) {
+      return res.status(400).json({ error: 'The "done" attribute is required in the request body' });
     }
 
-    if (deleted !== undefined) {
-      updateFields.deleted = deleted;
-    }
-
-    if (Object.keys(updateFields).length === 0) {
-      return res.status(400).json({ error: 'No valid fields provided for update' });
-    }
-
+    // Find the task by ID and update the 'done' attribute
     const updatedTask = await Tasks.findByIdAndUpdate(
       taskId,
-      updateFields,
-      { new: true } 
+      { $set: { done } },
+      { new: true }
     );
 
     if (!updatedTask) {
       return res.status(404).json({ error: 'Task not found' });
     }
 
-    res.status(200).json({
-      _id: updatedTask._id,
-      done: updatedTask.done,
-      deleted: updatedTask.deleted,
-    });
+    res.status(200).json(updatedTask);
   } catch (error) {
     console.error('Error updating task:', error);
     res.status(500).json({ error: 'Error processing the request' });
