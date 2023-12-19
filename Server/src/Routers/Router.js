b1938dc9146a5e1dfc3a45ea3d5a100d6f7abeba
GPrensLuna <prens.giova12@gmail.com>
2023-12-18 20:23:17 -0500

    tasks

--- Server/src/Routers/Router.js
+++ Server/src/Routers/Router.js
@@ -16,7 +16,7 @@ router.post("/concept", post.postConcept)
 router.post("/tasks", post.postInsectTasks)
 router.post("/shopping", post.postInsectShopping)
 
-router.put("/type/:idTasks", put.putTasks)
+router.put("/tasks/:idTasks", put.putTasks)
 
 
 export default router;
