6f5d057ff5ef6c7b4f94d2bf50cd09d448ea4c72
GPrensLuna <prens.giova12@gmail.com>
2024-01-10 21:05:40 -0500

    ♻️ Actualizacino de ventanas modales

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,14 +4,14 @@ import * as components from "./Components";
 
 function App() {
   return (
-    <div className="bg-slate-200 h-screen">
+    <main className="bg-slate-200 h-screen">
       <components.Navbar />
       <Routes>
         <Route path="/" element={<layouts.Home />}></Route>
         <Route path="/Insect" element={<layouts.InsectData />}></Route>
         <Route path="/Dashboard" element={<layouts.Dashboard />}></Route>
       </Routes>
-    </div>
+    </main>
   );
 }
 
--- Client/src/layouts/Home/Home.jsx
+++ Client/src/layouts/Home/Home.jsx
@@ -30,32 +30,30 @@ export const Home = () => {
   if (errorTasks || errorShopping)
     return <div>Error: {errorTasks?.message || errorShopping?.message}</div>;
 
-  const handleUpdate = async () => {
-    // Esta función se puede ajustar para actualizar los datos si es necesario
-  };
+  const handleUpdate = async () => {};
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-5 w-full">
-      <div className="lg:col-span-4">
-        <div className="flex justify-center text-[48px]">
+    <section className="grid grid-cols-1 lg:grid-cols-5 w-full">
+      <article className="lg:col-span-4">
+        <header className="flex justify-center text-[48px]">
           <h1 className="justify-center">INSECT PAYMENTS MADE</h1>
-        </div>
+        </header>
         <Components.InsectForm handleUpdate={handleUpdate} />
-        <div className="overflow-hidden m-3 rounded-md shadow-md">
+        <section className="overflow-hidden m-3 rounded-md shadow-md">
           <Components.Dashboard
             shoppingData={shoppingData}
             handleUpdate={handleUpdate}
           />
-        </div>
-      </div>
-      <div className="lg:w-full h-screen border-slate-800 border-l-2 p-4 overflow-y-auto text-center">
-        <h1 className="text-2xl font-bold border-b-2">PENDING TASKS</h1>
+        </section>
+      </article>
+      <aside className="lg:w-full overflow-y-auto text-center">
+        <h2 className="text-2xl font-bold border-b-2">PENDING TASKS</h2>
         <Components.Reminder
           tasksData={tasksData}
           handleUpdateTasks={"handleUpdateTasks"}
         />
-      </div>
-    </div>
+      </aside>
+    </section>
   );
 };
 
